33b78efc4eddb0271649cfcaa73d73b06d08348c
vulcu <winryvulcu@gmail.com>
2018-03-22 11:18:58 -0400

    fixed indent inconsistency (tabs converted to spaces)

--- WaveshapeDistortion.c
+++ WaveshapeDistortion.c
@@ -12,7 +12,7 @@
 // HAVE NOTHING TO DO WITH IT. LAST BUT NOT LEAST, BY USING THIS PLUG-IN
 // YOU RELINQUISH YOUR CLAIM TO SUE IT'S AUTHOR, AS WELL AS THE CLAIM TO
 // ENTRUST SOMEBODY ELSE WITH DOING SO.
-// 
+//
 // Released under GPL:
 // <http://www.gnu.org/licenses/>.
 //
@@ -49,12 +49,12 @@
 
 // Define User controls as GPIO Input Pins
 #define PUSH1 			    RPI_GPIO_P1_08      //GPIO14
-#define PUSH2 			    RPI_V2_GPIO_P1_38  	//GPIO20 
+#define PUSH2 			    RPI_V2_GPIO_P1_38  	//GPIO20
 #define TOGGLE_SWITCH 	    RPI_V2_GPIO_P1_32 	//GPIO12
 #define FOOT_SWITCH         RPI_GPIO_P1_10 	    //GPIO15
 #define LED   			    RPI_V2_GPIO_P1_36 	//GPIO16
 
-static uint8_t mosi[10] = { 0x01, 0x00, 0x00 }; //12 bit ADC read 0x08 ch0, - 0c for ch1 
+static uint8_t mosi[10] = { 0x01, 0x00, 0x00 }; //12 bit ADC read 0x08 ch0, - 0c for ch1
 static uint8_t miso[10] = { 0 };
 
 static uint8_t FOOT_SWITCH_val;
@@ -99,164 +99,164 @@ static float_t otm = 0;
 static float_t OutputLevel = 0;
 
 static void configurePWM(void) {
-	//define PWM output configuration
-    bcm2835_gpio_fsel(18,BCM2835_GPIO_FSEL_ALT5 ); //PWM0 signal on GPIO18    
-    bcm2835_gpio_fsel(13,BCM2835_GPIO_FSEL_ALT0 ); //PWM1 signal on GPIO13    
-	bcm2835_pwm_set_clock(2); // Max clk frequency (19.2MHz/2 = 9.6MHz)
-    bcm2835_pwm_set_mode(0,1 , 1); //channel 0, markspace mode, PWM enabled. 
-	bcm2835_pwm_set_range(0,64);   //channel 0, 64 is max range (6bits): 9.6MHz/64=150KHz switching PWM freq.
+    //define PWM output configuration
+    bcm2835_gpio_fsel(18,BCM2835_GPIO_FSEL_ALT5 ); //PWM0 signal on GPIO18
+    bcm2835_gpio_fsel(13,BCM2835_GPIO_FSEL_ALT0 );              //PWM1 signal on GPIO13
+    bcm2835_pwm_set_clock(2); // Max clk frequency (19.2MHz/2 = 9.6MHz)
+    bcm2835_pwm_set_mode(0,1 , 1); //channel 0, markspace mode, PWM enabled.
+    bcm2835_pwm_set_range(0,64);   //channel 0, 64 is max range (6bits): 9.6MHz/64=150KHz switching PWM freq.
     bcm2835_pwm_set_mode(1, 1, 1); //channel 1, markspace mode, PWM enabled.
-	bcm2835_pwm_set_range(1,64);   //channel 0, 64 is max range (6bits): 9.6MHz/64=150KHz switching PWM freq.
+    bcm2835_pwm_set_range(1,64);   //channel 0, 64 is max range (6bits): 9.6MHz/64=150KHz switching PWM freq.
 }
 
 static void configureSPI(void) {
-	//define SPI bus configuration
-    bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
-    bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                   // The default
-    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_64); 	  // 4MHz clock/64 = 62500
-    bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
-    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
+    //define SPI bus configuration
+    bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);    // The default
+    bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                 // The default
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_64); 	// 4MHz clock/64 = 62500
+    bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                    // The default
+    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);    // the default
 }
 
 static void setupGPIO(void) {
     //Define GPIO pin configuration
-    bcm2835_gpio_fsel(PUSH1, BCM2835_GPIO_FSEL_INPT); 			      //PUSH1 button as input
-  	bcm2835_gpio_fsel(PUSH2, BCM2835_GPIO_FSEL_INPT); 			      //PUSH2 button as input
-	bcm2835_gpio_fsel(TOGGLE_SWITCH, BCM2835_GPIO_FSEL_INPT);	    //TOGGLE_SWITCH as input
-  	bcm2835_gpio_fsel(FOOT_SWITCH, BCM2835_GPIO_FSEL_INPT); 	    //FOOT_SWITCH as input
-  	bcm2835_gpio_fsel(LED, BCM2835_GPIO_FSEL_OUTP);				        //LED as output
+    bcm2835_gpio_fsel(PUSH1, BCM2835_GPIO_FSEL_INPT); 			//PUSH1 button as input
+    bcm2835_gpio_fsel(PUSH2, BCM2835_GPIO_FSEL_INPT); 			//PUSH2 button as input
+    bcm2835_gpio_fsel(TOGGLE_SWITCH, BCM2835_GPIO_FSEL_INPT);	//TOGGLE_SWITCH as input
+    bcm2835_gpio_fsel(FOOT_SWITCH, BCM2835_GPIO_FSEL_INPT); 	//FOOT_SWITCH as input
+    bcm2835_gpio_fsel(LED, BCM2835_GPIO_FSEL_OUTP);				//LED as output
 
-    bcm2835_gpio_set_pud(PUSH1, BCM2835_GPIO_PUD_UP);             //PUSH1 pull-up enabled
-  	bcm2835_gpio_set_pud(PUSH2, BCM2835_GPIO_PUD_UP);             //PUSH2 pull-up enabled
-  	bcm2835_gpio_set_pud(TOGGLE_SWITCH, BCM2835_GPIO_PUD_UP);     //TOGGLE_SWITCH pull-up enabled
-  	bcm2835_gpio_set_pud(FOOT_SWITCH, BCM2835_GPIO_PUD_UP);       //FOOT_SWITCH pull-up enabled
+    bcm2835_gpio_set_pud(PUSH1, BCM2835_GPIO_PUD_UP);           //PUSH1 pull-up enabled
+    bcm2835_gpio_set_pud(PUSH2, BCM2835_GPIO_PUD_UP);           //PUSH2 pull-up enabled
+    bcm2835_gpio_set_pud(TOGGLE_SWITCH, BCM2835_GPIO_PUD_UP);   //TOGGLE_SWITCH pull-up enabled
+    bcm2835_gpio_set_pud(FOOT_SWITCH, BCM2835_GPIO_PUD_UP);     //FOOT_SWITCH pull-up enabled
 }
 
 // cube function
 static float cubef(float x) {
-	return (x * x * x);
+    return (x * x * x);
 }
 
 // hard clip of input signal
 static float HardClip(float sample, float thresh) {
-  return 0.5 * (fabsf(sample + thresh) - fabsf(sample - thresh));
+    return 0.5 * (fabsf(sample + thresh) - fabsf(sample - thresh));
 };
 
 // cubic soft clip function
 static float SoftCubicClip(float sample, float thresh) {
-  float threshInv = 1 / thresh;
-  return threshInv * ((thresh * 1.5 * HardClip(sample, thresh)) -
-    (0.5 * cubef(HardClip(sample, thresh)) * threshInv));
+    float threshInv = 1 / thresh;
+    return threshInv * ((thresh * 1.5 * HardClip(sample, thresh)) -
+        (0.5 * cubef(HardClip(sample, thresh)) * threshInv));
 };
 
 // use this to process audio via the SoftCubicClip algorithm
 static float SoftCubic(float sample) {
-  return invSqrtOf2 * (SoftCubicClip(sample, CubicSoftClipThreshold) + 
-    (CubicHarmonicBalance * SoftCubicClip(fabsf(sample), CubicSoftClipThreshold)));
+    return invSqrtOf2 * (SoftCubicClip(sample, CubicSoftClipThreshold) +
+        (CubicHarmonicBalance * SoftCubicClip(fabsf(sample), CubicSoftClipThreshold)));
 };
 
 // soft clip function with adjustable knee
 static float SKClip(float sample, float knee) {
-  return sample / (knee * fabsf(sample) + 1);
+    return sample / (knee * fabsf(sample) + 1);
 };
 
 // use this to process audio via the SKClip algorithm
 static float SoftKnee(float sample) {
-  SKClip(sample, SoftClipKnee) + ((SoftClipKnee / 2) * SKClip(fabsf(sample), SoftClipKnee));
+    SKClip(sample, SoftClipKnee) + ((SoftClipKnee / 2) * SKClip(fabsf(sample), SoftClipKnee));
 };
 
 // use this to process audio via the leaky integrator algorithm
 static float LeakyInt(float sample, float sampleLast) {
-  if (sample > sampleLast) {
-    return invSqrtOf2 * ((1 - TcRise) * sample) + (TcRise * sampleLast);
-	}
-  else {
-    return invSqrtOf2 * ((1 - TcFall) * sample) + (TcFall * sampleLast);
-	}
+    if (sample > sampleLast) {
+        return invSqrtOf2 * ((1 - TcRise) * sample) + (TcRise * sampleLast);
+        }
+      else {
+       return invSqrtOf2 * ((1 - TcFall) * sample) + (TcFall * sampleLast);
+       }
 };
 
 // apply a DC blocker to processed audio
 static float BlockDC(float sample) {
-  otm = DC_Cutoff * otm + sample - itm;
-  itm = sample;
-  sample = otm;
-  return sample;
+    otm = DC_Cutoff * otm + sample - itm;
+    itm = sample;
+    sample = otm;
+    return sample;
 };
 
 int main(void) {
     // Try to start the BCM2835 Library to access GPIO.
   if (!bcm2835_init()) {
-		printf("bcm2835_init failed. Are you running as root??\n");
-      	return 1;
-	}
-
-	// Try to start the BCM2835 SPI bus.
-	if (!bcm2835_spi_begin()) {
-		printf("bcm2835_spi_begin failed. Are you running as root??\n");
-    	return 1;
-	}
-
-	// Main Loop
-	while(1) {
-		//read 12 bits ADC
-		bcm2835_spi_transfernb(mosi, miso, 3);
-		input_signal = miso[2] + ((miso[1] & 0x0F) << 8); 
-
-		//Read the PUSH buttons every 15625 times (0.250s) to save resources.
-		read_timer++;
-		if (read_timer==15625) {
-			read_timer=0;
-			PUSH1_val = bcm2835_gpio_lev(PUSH1);
-			PUSH2_val = bcm2835_gpio_lev(PUSH2);
-			TOGGLE_SWITCH_val = bcm2835_gpio_lev(TOGGLE_SWITCH);
-			FOOT_SWITCH_val = bcm2835_gpio_lev(FOOT_SWITCH);
-
-			//light the effect when the footswitch is activated.
-			bcm2835_gpio_write(LED,!FOOT_SWITCH_val); 
-			
-			//update booster_value when the PUSH1 or 2 buttons are pushed.
-			if (PUSH1_val==0) {
-				bcm2835_delay(100); //100ms delay for buttons debouncing
-				if (waveshapeType > 0) {
-					waveshapeType = (enum waveshapers)((uint8_t)waveshapeType - 1);
-				}
-				else {
-					waveshapeType = (enum waveshapers) 2;
-				}
-			}
-			else if (PUSH2_val==0) {
-				bcm2835_delay(100); //100ms delay for buttons debouncing.
-				if (waveshapeType < 0) {
-					waveshapeType = (enum waveshapers)((uint8_t)waveshapeType + 1);
-				}
-				else {
-					waveshapeType = (enum waveshapers) 0;
-				}
-			}
-		}
-
-		//**** WAVESHAPE DISTORTION ***///
-		switch (waveshapeType) {
-			case leakyIntegrator:
-			   	output_signal = BlockDC(input_signal);
-				break;
-			case softKnee:
-			   	output_signal = input_signal;
-				break;
-			case cubic:
-			   	output_signal = input_signal;
-				break;
-			default:
-				output_signal = input_signal;
-		}
-		
-		//generate two 6-bit PWM outputs to simulate 12-bit PWM
-		bcm2835_pwm_set_data(1, output_signal & 0x3F);
-		bcm2835_pwm_set_data(0, output_signal >> 6);
+        printf("bcm2835_init failed. Are you running as root??\n");
+          return 1;
     }
-	
-	//close all and exit (does this actually work?)
-	bcm2835_spi_end();
+
+    // Try to start the BCM2835 SPI bus.
+    if (!bcm2835_spi_begin()) {
+        printf("bcm2835_spi_begin failed. Are you running as root??\n");
+        return 1;
+    }
+
+    // Main Loop
+    while(1) {
+        //read 12 bits ADC
+        bcm2835_spi_transfernb(mosi, miso, 3);
+        input_signal = miso[2] + ((miso[1] & 0x0F) << 8);
+
+        //Read the PUSH buttons every 15625 times (0.250s) to save resources.
+        read_timer++;
+        if (read_timer==15625) {
+            read_timer=0;
+            PUSH1_val = bcm2835_gpio_lev(PUSH1);
+            PUSH2_val = bcm2835_gpio_lev(PUSH2);
+            TOGGLE_SWITCH_val = bcm2835_gpio_lev(TOGGLE_SWITCH);
+            FOOT_SWITCH_val = bcm2835_gpio_lev(FOOT_SWITCH);
+
+            //light the effect when the footswitch is activated.
+            bcm2835_gpio_write(LED,!FOOT_SWITCH_val);
+
+            //update booster_value when the PUSH1 or 2 buttons are pushed.
+            if (PUSH1_val==0) {
+                bcm2835_delay(100); //100ms delay for buttons debouncing
+                if (waveshapeType > 0) {
+                    waveshapeType = (enum waveshapers)((uint8_t)waveshapeType - 1);
+                }
+                else {
+                    waveshapeType = (enum waveshapers) 2;
+                }
+            }
+            else if (PUSH2_val==0) {
+                bcm2835_delay(100); //100ms delay for buttons debouncing.
+                if (waveshapeType < 0) {
+                    waveshapeType = (enum waveshapers)((uint8_t)waveshapeType + 1);
+                }
+                else {
+                    waveshapeType = (enum waveshapers) 0;
+                }
+            }
+        }
+
+        //**** WAVESHAPE DISTORTION ***///
+        switch (waveshapeType) {
+            case leakyIntegrator:
+                   output_signal = BlockDC(input_signal);
+                break;
+            case softKnee:
+                   output_signal = input_signal;
+                break;
+            case cubic:
+                   output_signal = input_signal;
+                break;
+            default:
+                output_signal = input_signal;
+        }
+
+        //generate two 6-bit PWM outputs to simulate 12-bit PWM
+        bcm2835_pwm_set_data(1, output_signal & 0x3F);
+        bcm2835_pwm_set_data(0, output_signal >> 6);
+    }
+
+    //close all and exit (does this actually work?)
+    bcm2835_spi_end();
     bcm2835_close();
     return 0;
 }
